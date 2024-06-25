5bb404552f007dd89250744abb1f21ddeabe9014
EmizhAlan <150579465+EmizhAlan@users.noreply.github.com>
2024-06-26 00:54:11 +0300

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,1 +1,15 @@
-
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "src/index.js",
+      "use": "@vercel/node"
+    }
+  ],
+  "routes": [
+    { "src": "/repositories", "dest": "/src/index.js" },
+    { "src": "/repositories/:id", "dest": "/src/index.js" },
+    { "src": "/sync", "dest": "/src/index.js" },
+    { "src": "/(.*)", "dest": "/src/index.js" }
+  ]
+}
